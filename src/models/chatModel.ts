e1a767fa2dc029d98f1b14f44c984557fe499e22
David Eglin <david.eglin@and.digital>
2024-06-21 13:21:27 +0100

    update chat model to remove error

--- src/models/chatModel.ts
+++ src/models/chatModel.ts
@@ -1,44 +1,24 @@
 import axios from 'axios';
 import { simulateDelay } from '@/src/utils/simulateDelay/simulateDelay';
 import { FetchMessagesResponse, MessageData } from '../app/chat/chat.types';
+
 import { Amplify } from 'aws-amplify';
-import { post } from 'aws-amplify/api';
+
+Amplify.configure({})
 
 const API_URL = '/api/messages';
 
-Amplify.configure({
-  API: {
-    REST: {
-      'rca-youngvic-PWA-code': {
-        endpoint: API_URL,
-      },
-    },
-  },
-});
-
-export const fetchMessages = async (query: MessageData) => {
+export const fetchMessages = async (query: MessageData): Promise<string> => {
   try {
-    const restOperation = post({
-      apiName: 'rca-youngvic-PWA-code',
-      path: API_URL,
-      options: {
-        body: query,
-      },
-    });
-    // const response = await axios.post<FetchMessagesResponse>(API_URL, query);
-    // const messages = response.data.parts;
-    // let fullMessage = '';
-    // for (let part of Object.keys(messages)) {
-    //   fullMessage += `${messages[part]} \n`;
-    // }
-    // await simulateDelay({ message: fullMessage });
+    const response = await axios.post<FetchMessagesResponse>(API_URL, query);
+    const messages = response.data.parts;
+    let fullMessage = '';
+    for (let part of Object.keys(messages)) {
+      fullMessage += `${messages[part]} \n`;
+    }
+    await simulateDelay({ message: fullMessage });
 
-    // return fullMessage;
-
-    const { body } = await restOperation.response;
-    const response = await body.json();
-
-    return response;
+    return fullMessage;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       // Handle Axios error
